6cef802b462c64c38631189de71a5aa931c7223f
Yashin Ilya <imyashin1996@gmail.com>
2022-03-29 15:46:39 +0300

    Добавлена возможность изменения пароля

--- bg_project/apps/users/urls.py
+++ bg_project/apps/users/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import (LogoutView,
+                                       PasswordChangeView,
+                                       PasswordChangeDoneView, )
 
 from . import views
 
 urlpatterns = [
     path("login/", views.log_in, name="log_in"),
     path("logout/", LogoutView.as_view(), name="log_out"),
+    path("password_change/", PasswordChangeView.as_view(template_name='users/password_change.html'),
+         name='password_change'),
+    path("password_change_done/", PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+         name='password_change_done'),
     path("signup/", views.sign_up, name="sign_up"),
     path("signup-confirm/", views.confirm_signup, name="confirm_sign_up"),
 ]
